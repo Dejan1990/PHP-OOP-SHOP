1bdf66e12f1ccf21df6865f2edb9cae6548b5da0
Dejan1990 <dejan.simo@hotmail.com>
2023-08-07 13:46:49 +0200

    Database connection - OOP way

--- app/config/config.php
+++ app/config/config.php
@@ -5,8 +5,15 @@ $db_username = 'root';
 $db_password = 'tiger';
 $database_name = 'shop_oop_db';
 
-$conn = mysqli_connect($server_name, $db_username, $db_password, $database_name);
-
-if (!$conn) {
+//$conn = mysqli_connect($server_name, $db_username, $db_password, $database_name);
+/*if (!$conn) {
     echo "Something went wrong";
+}*/
+
+$conn = new Mysqli($server_name, $db_username, $db_password, $database_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+
+
