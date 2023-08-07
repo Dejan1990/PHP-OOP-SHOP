2c5ef600de6a6c4021fff756264c615435f106b5
Dejan1990 <dejan.simo@hotmail.com>
2023-08-07 13:42:04 +0200

    Database connection

--- app/config/config.php
+++ app/config/config.php
@@ -1,2 +1,12 @@
-<?php 
+<?php
 
+$server_name = 'database';
+$db_username = 'root';
+$db_password = 'tiger';
+$database_name = 'shop_oop_db';
+
+$conn = mysqli_connect($server_name, $db_username, $db_password, $database_name);
+
+if (!$conn) {
+    echo "Something went wrong";
+}
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 
+require_once "app/config/config.php";
+
 echo "Hello World";
+
