15752538ae38636b451e8f75c9405ade4782aa4d
Dejan1990 <dejan.simo@hotmail.com>
2023-08-29 14:28:58 +0200

    Add login logic

--- app/classes/User.php
+++ app/classes/User.php
@@ -29,4 +29,25 @@ class User
             return false;
         }
     }
+
+    public function login($username, $password)
+    {
+        $sql = "SELECT user_id, password FROM users WHERE username = ?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bind_param("s", $username);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows == 1) {
+            $user = $result->fetch_assoc();
+
+            if (password_verify($password, $user['password'])) {
+                $_SESSION['user_id'] = $user['user_id'];
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- login.php
+++ login.php
@@ -1,8 +1,32 @@
+<?php
+require_once "app/config/config.php";
+require_once "app/classes/User.php";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user = new User();
+    $result = $user->login($username, $password);
+
+    if (!$result) {
+        $_SESSION['message']['type'] = 'danger';
+        $_SESSION['message']['text'] = 'Netacan username ili sifra!';
+        header("Location: login.php");
+        exit();
+    }
+
+    $_SESSION['message']['type'] = 'success';
+    $_SESSION['message']['text'] = 'Uspesno ste se ulogovali!';
+    header("Location: index.php");
+    exit();
+}
+?>
 <?php require_once "inc/header.php"; ?>
 <div class="row justify-content-center">
     <div class="col-lg-5">
         <h3 class="text-center py-5">Login</h3>
-        <form method="post" action="" class="mb-3">
+        <form method="POST" action="" class="mb-3">
             <div class="form-group mb-3">
                 <label for="username" class="form-label">Username</label>
                 <input type="text" class="form-control" id="username" name="username" required>
