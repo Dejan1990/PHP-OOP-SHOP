9a6f65d135ce8b9943a75f7b862f3f4814ad751a
Dejan1990 <dejan.simo@hotmail.com>
2023-08-31 12:54:08 +0200

    Style checkout page

---
+++ checkout.php
@@ -0,0 +1,36 @@
+<?php
+require_once "app/config/config.php";
+require_once "app/classes/Cart.php";
+require_once "app/classes/User.php";
+
+$user = new User();
+if (!$user->is_logged()) {
+    header("Location: login.php");
+    exit();
+}
+
+$cart = new Cart();
+$cart_items = $cart->get_cart_items(); 
+?>
+
+<?php require_once "inc/header.php"; ?>
+<form method="POST" action="">
+    <div class="form-group mb-3">
+        <label for="country">Drzava</label>
+        <input type="text" class="form-control" id="country" name="country" required>
+    </div>
+    <div class="form-group mb-3">
+        <label for="city">Grad</label>
+        <input type="text" class="form-control" id="city" name="city" required>
+    </div>
+    <div class="form-group mb-3">
+        <label for="zip">Postanski broj</label>
+        <input type="text" class="form-control" id="zip" name="zip" required>
+    </div>
+    <div class="form-group mb-3">
+        <label for="address">Adresa</label>
+        <input type="text" class="form-control" id="address" name="address" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Order</button>
+</form>
+<?php require_once "inc/footer.php"; ?>
